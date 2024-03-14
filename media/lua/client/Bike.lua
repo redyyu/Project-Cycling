97a14eb93766ddd8ac31b44680713a7f5121785a
redyyu <redy.ru@gmail.com>
2024-03-14 23:21:56 +0800

    fix

--- media/lua/client/Bike.lua
+++ media/lua/client/Bike.lua
@@ -1,5 +1,6 @@
 require "TimedActions/ISTimedActionQueue"
 require "TimedActions/ISInventoryTransferAction"
+require "ISUI/ISInventoryPaneContextMenu"
 
 local seatNameTable = {"SeatFrontLeft", "SeatFrontRight", "SeatMiddleLeft", "SeatMiddleRight", "SeatRearLeft", "SeatRearRight"}
 local soundFX = {
@@ -104,7 +105,7 @@ Bike.onPlayerUpdate = function (playerObj)
     local handItem = playerObj:getPrimaryHandItem()
     local equippedBike = false
 
-    for _, item in ipairs(Bike.getBikesFromInvertory(playerInv)) do
+    for idx, item in ipairs(Bike.getBikesFromInvertory(playerInv)) do
         -- DO NOT AUTO equipped, it will cause lot more logic prolbem,
         -- such as conflict with other MOD did samething.
         -- item will keep equip/unequip in millseconds, don't even see the action.
@@ -116,12 +117,12 @@ Bike.onPlayerUpdate = function (playerObj)
         --     playerObj:setSecondaryHandItem(item)
         --     equippedBike = item
         -- end
-
         -- drop any Bike not equipped.
         if item:isEquipped() then
             equippedBike = item
         else
             Bike.dropItemInsanely(playerObj, item)
+            playerObj:Say(getText('IGUI_PlayerText_Cant_Take_Bike_This_Way'))
         end
     end
 
@@ -204,16 +205,25 @@ Bike.onEnterVehicle = function (playerObj)
 end
 
 
-Bike.onEquipBike = function (playerNum, WItem)
+Bike.onEquipBike = function (playerNum, bike)
     local playerObj = getSpecificPlayer(playerNum)
-    if WItem:getSquare() and luautils.walkAdj(playerObj, WItem:getSquare()) then
+    local walk_to = nil
+    if bike:getWorldItem() then
+        walk_to = luautils.walkAdj(playerObj, bike:getWorldItem():getSquare())
+    elseif bike:getContainer() then
+        walk_to = luautils.walkToContainer(bike:getContainer(), playerObj:getPlayerNum())
+    else
+        walk_to = luautils.walkAdj(playerObj, playerObj:getCurrentSquare())
+    end
+    
+    if walk_to then
         if playerObj:getPrimaryHandItem() then
             ISTimedActionQueue.add(ISUnequipAction:new(playerObj, playerObj:getPrimaryHandItem(), 50));
         end
         if playerObj:getSecondaryHandItem() and playerObj:getSecondaryHandItem() ~= playerObj:getPrimaryHandItem() then
             ISTimedActionQueue.add(ISUnequipAction:new(playerObj, playerObj:getSecondaryHandItem(), 50));
         end
-        ISTimedActionQueue.add(ISTakeBike:new(playerObj, WItem, 50))
+        ISTimedActionQueue.add(ISTakeBike:new(playerObj, bike, 50))
     end
 end
 
@@ -243,32 +253,16 @@ Bike.doFillWorldObjectContextMenu = function (playerNum, context, worldobjects, 
 
         for _, obj in ipairs(worldObjTable) do
             local item = obj:getItem()
-            if item and item:hasTag('Bike') then
+            if item and item:hasTag('Bike') and not playerObj:isHandItem(item)then
                 local old_option = context:getOptionFromName(getText("ContextMenu_Grab"))
                 if old_option then
                     context:removeOptionByName(old_option.name)
-                    context:addOptionOnTop(getText("ContextMenu_GET_ON_BIKE"), playerNum, Bike.onEquipBike, obj)
+                    context:addOptionOnTop(getText("ContextMenu_TAKE_RIDE", item:getDisplayName()), playerNum, Bike.onEquipBike, item)
                     return
                 end                
             end
         end
     end
-end
-
-
-Bike.onGrabBikeFromContainer = function (playerObj, bike)
-    local playerObj = getSpecificPlayer(playerNum)
-    local container = bike:getContainer()
-    local inventory = getPlayerInventory(playerNum).inventory
-
-    if bike:getContainer() ~= inventory and inventory:hasRoomFor(playerObj, bike) then
-        if luautils.walkToContainer(bike:getContainer(), playerNum) then
-            ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, bike, bike:getContainer(), inventory))
-        end
-    else
-        ISInventoryPaneContextMenu.dropItem(bike, playerNum)
-    end
-    
 end
 
 
@@ -285,21 +279,16 @@ Bike.doInventoryContextMenu = function (playerNum, context, items)
 
             -- local old_option = context:getOptionFromName(getText("ContextMenu_Grab"))
             -- NO Need this, `HeavyItem` don't have `Grab` option in Inventory.
-
-            if item == playerObj:getPrimaryHandItem() then
+            
+            if playerObj:isHandItem(item) then
                 -- replace native Drop
                 context:removeOptionByName(getText("ContextMenu_Drop"))
                 context:addOptionOnTop(getText("ContextMenu_GET_OFF_BIKE"), playerNum, Bike.onUnequipBike, item)
                 return
             else
                 -- context:removeOptionByName(old_option.name)
-                if item:getContainer():getType() == "floor" then
-                    context:addOptionOnTop(getText("ContextMenu_GET_ON_BIKE"), playerNum, Bike.onEquipBike, item:getWorldItem())
-                    return
-                else
-                    context:addOptionOnTop(getText("ContextMenu_GET_ON_BIKE"), playerNum, Bike.onGrabBikeFromContainer, item)
-                    return
-                end
+                context:addOptionOnTop(getText("ContextMenu_TAKE_RIDE", item:getDisplayName()), playerNum, Bike.onEquipBike, item)
+                return
             end
         end
     end
--- media/lua/client/TimedActions/ISTakeBike.lua
+++ media/lua/client/TimedActions/ISTakeBike.lua
@@ -10,7 +10,7 @@ ISTakeBike = ISBaseTimedAction:derive("ISTakeBike");
 
 function ISTakeBike:isValid()
     -- Check that the item wasn't picked up by a preceding action
-    if self.item == nil or self.wItem:getSquare() == nil then return false end
+    if self.item == nil then return false end
     return true
 end
 
@@ -22,7 +22,7 @@ function ISTakeBike:start()
     self:setActionAnim("Loot");
     self:setAnimVariable("LootPosition", "Low");
     self:setOverrideHandModels(nil, nil);
-    self.item:setJobType(getText("ContextMenu_GET_ON_BIKE"));
+    self.item:setJobType(getText("ContextMenu_TAKE_RIDE"));
     self.item:setJobDelta(0.0);
 end
 
@@ -36,37 +36,39 @@ function ISTakeBike:stop()
 end
 
 function ISTakeBike:perform()
-    -- forceDropHeavyItems(self.character)
+    forceDropHeavyItems(self.character)
     -- from TimedActions/ISEquipWeaponAction.lua drop Corps and Generator or any other item hasTag `HeavyItem`
-    self.wItem:getSquare():transmitRemoveItemFromSquare(self.wItem);
-    self.wItem:removeFromWorld()
-    self.wItem:removeFromSquare()
-    self.wItem:setSquare(nil)
-    self.item:setWorldItem(nil)
-    self.item:setJobDelta(0.0);
-    self.character:getInventory():setDrawDirty(true);
-    self.character:getInventory():AddItem(self.item);
-    self.action:stopTimedActionAnim();
-    self.action:setLoopedAction(false);
-    self.character:setPrimaryHandItem(self.item);
-    self.character:setSecondaryHandItem(self.item);
-    local pdata = getPlayerData(self.character:getPlayerNum());
+    if self.worldItem then
+        self.worldItem:getSquare():transmitRemoveItemFromSquare(self.worldItem)
+        self.worldItem:removeFromWorld()
+        self.worldItem:removeFromSquare()
+        self.worldItem:setSquare(nil)
+        self.item:setWorldItem(nil)
+    end
+    self.item:setJobDelta(0.0)
+    self.character:getInventory():setDrawDirty(true)
+    self.character:getInventory():AddItem(self.item)
+    self.action:stopTimedActionAnim()
+    self.action:setLoopedAction(false)
+    self.character:setPrimaryHandItem(self.item)
+    self.character:setSecondaryHandItem(self.item)
+    local pdata = getPlayerData(self.character:getPlayerNum())
     if pdata ~= nil then
-        pdata.playerInventory:refreshBackpacks();
-        pdata.lootInventory:refreshBackpacks();
+        pdata.playerInventory:refreshBackpacks()
+        pdata.lootInventory:refreshBackpacks()
     end
     -- needed to remove from queue / start next.
     ISBaseTimedAction.perform(self);
 
 end
 
-function ISTakeBike:new (character, wItem, time)
+function ISTakeBike:new (character, item, time)
     local o = {}
     setmetatable(o, self)
     self.__index = self
     o.character = character
-    o.wItem = wItem
-    o.item = wItem:getItem()
+    o.item = item
+    o.worldItem = item:getWorldItem()
     o.stopOnWalk = true
     o.stopOnRun = true   
     o.maxTime = time;
